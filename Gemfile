b7f7d676b7e0dfb60dcbd2000b142c2d9354b387
Alex Cortes <alexcortes247@gmail.com>
2017-04-24 14:27:50 -0600

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.1.0'
 gem 'devise', '~> 4.2', '>= 4.2.1'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem 'cortes_view_tool', git: 'https://github.com/smartalex24/cortes_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,5 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    CortesViewTool::Renderer.copyright 'Alex Cortes', 'All rights reserved'
+  end
 end
 
