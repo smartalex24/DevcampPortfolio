827ad68fb5d7390c319e1e06922b9afb53347bde
Alex Cortes <alexcortes247@gmail.com>
2017-05-01 10:26:09 -0600

    Updated file upload forms and refractored placeholder functionality into view

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,10 +1,3 @@
 class Skill < ApplicationRecord
-  include Placeholder
 	validates_presence_of  :title, :percent_utilized
-
-  after_initialize :set_defaults 
-
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '350', width: '350')
-  end
 end
